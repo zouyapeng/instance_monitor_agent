157a88d7677c1f151a27a2b906b8acf010d4c634
zouyapeng <zyp19901009@163.com>
2016-12-09 14:00:24 +0800

    0.1.7: 1. fixed error with unable to start in 0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name='VMAgent',
-      version='0.1.6',
+      version='0.1.7',
       description='Openstack instance monitor agent',
       url='http://github.com/zouyapeng/instance_monitor_agent',
       author='Zouyapeng',
